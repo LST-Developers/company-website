56382ef85c708f90164ebd6dd173324a48ff2958
RicardoRL1 <ricardopedrozoap@gmail.com>
2023-02-15 12:56:52 -0500

    se agregaron iconos de paginas más confiables

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -11,11 +11,11 @@ function NavBar() {
                   <li><a href="#">Sobre Nosotros</a></li>
                   <li><a href="#">Contactanos</a></li>
                 </ul>
-                <img class="iconSearch" src="https://assets.stickpng.com/images/59cfc4d2d3b1936210a5ddc7.png" alt="icono buscar" width="40"height="40" />
+                <span class="material-icons-outlined">search</span>
                 <div class="userOption">
-                  <img class="iconUser" src="https://assets.stickpng.com/images/585e4be1cb11b227491c3398.png" alt="icono de usuario" width="40" height="40" />
+                 <span class="material-icons-outlined">account_circle</span>
                   <span>Nobre de Usuario</span>
-                  <img src="https://static.vecteezy.com/system/resources/thumbnails/000/575/331/small/vector60-5811-01.jpg" alt="icono de cerrar sesion" width="50" height="50"/>
+                  <span class="material-icons-outlined">logout</span>
                 </div>         
       </nav>
         </div>  
