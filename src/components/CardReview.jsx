3cb5a0fb94a2e2b196c01c2344b505b7da1a453b
Andrés Orozco <andresorozcog2@gmail.com>
2023-02-20 19:19:58 -0500

    Se realizó el componente de reviews

--- src/components/CardReview.jsx
+++ src/components/CardReview.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React from 'react';
 
-function CardReview() {
+function CardReview({imageUrl, rating, name, position}) {
+  const stars = [];
+  
+  for (let i = 1; i <= 5; i++) {
+    if (i <= rating) {
+      stars.push(<span key={i} className="star filled">&#9733;</span>);
+    } else {
+      stars.push(<span key={i} className="star">&#9734;</span>);
+    }
+  }
   return (
-    <div>CardReview</div>
+    <div className='card'>
+      <img src= {imageUrl} alt='user'></img>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique recusandae eos illum accusantium aliquam commodi quis, reiciendis dolores placeat qui quam autem. Voluptatum eius sint laborum quas! Accusamus, recusandae earum?</p>
+      <div className='star-rating'>
+        {stars}
+      </div>
+      <h3>{name}</h3>
+      <h4>{position}</h4>
+    </div>
   )
 }
 
