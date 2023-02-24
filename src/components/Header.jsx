ba0dca65a6cce83b21608de697536d7e22b67f08
Andrés Orozco <andresorozcog2@gmail.com>
2023-02-23 21:21:12 -0500

    Se actualizó el header para móviles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,6 +8,8 @@ function Header() {
         "Sobre Nosotros",
         "Contáctanos",
     ]);
+    const [isOpen, setIsOpen] = useState(false);
+    const [isMobile, setIsMobile] = useState(false);
 
     useEffect(() => {
         function handleScroll() {
@@ -27,21 +29,67 @@ function Header() {
         };
     }, []);
 
+    useEffect(() => {
+        const handleResize = () => {
+          setIsMobile(window.innerWidth <= 768);
+        };
+    
+        handleResize();
+    
+        window.addEventListener('resize', handleResize);
+    
+        return () => {
+          window.removeEventListener('resize', handleResize);
+        };
+      }, []);
+
     return (
-        <div
+        <div>
+        {isMobile ? (
+            <div
+            className="header"
+        >
+            <img
+                src="https://thumbs.dreamstime.com/b/letter-lst-simple-monogram-logo-icon-design-initial-vector-illustration-203570670.jpg"
+                alt="logo del equipo"
+                width="50"
+                height="50"
+                className="logo"
+            />
+            <ul className={`navBar ${isOpen ? "open" : ""}`}>
+                {routes.map((route) => {
+                    return (
+                        <li>
+                            <a href="#" style={{ color: "black" }}>
+                                {route}
+                            </a>
+                        </li>
+                    );
+                })}
+            </ul>
+            <div className="toggle" onClick={ () => setIsOpen(!isOpen)}>
+                <span class="material-icons-outlined">{!isOpen ? "menu" : "close"}</span>
+            </div>
+            <div class="userOption">
+                <span class="material-icons-outlined">search</span>
+                <span class="material-icons-outlined">account_circle</span>
+                <span className="userName">Nombre de Usuario</span>
+                <span class="material-icons-outlined">logout</span>
+            </div>
+        </div> )
+        : (
+            <div
             className="header"
             style={{ backgroundColor: headerColor, color: color }}
         >
-            <a href="#">
-                <img
-                    src="https://thumbs.dreamstime.com/b/letter-lst-simple-monogram-logo-icon-design-initial-vector-illustration-203570670.jpg"
-                    alt="logo del equipo"
-                    width="50"
-                    height="50"
-                    className="logo"
-                />
-            </a>
-            <ul class="navBar">
+            <img
+                src="https://thumbs.dreamstime.com/b/letter-lst-simple-monogram-logo-icon-design-initial-vector-illustration-203570670.jpg"
+                alt="logo del equipo"
+                width="50"
+                height="50"
+                className="logo"
+            />
+            <ul class={`navBar ${isOpen ? "open" : ""}`}>
                 {routes.map((route) => {
                     return (
                         <li>
@@ -51,13 +99,18 @@ function Header() {
                         </li>
                     );
                 })}
-                <span class="material-icons-outlined">search</span>
             </ul>
+            <div className="toggle" onClick={ () => setIsOpen(!isOpen)}>
+                <span class="material-icons-outlined">{!isOpen ? "menu" : "close"}</span>
+            </div>
             <div class="userOption">
+                <span class="material-icons-outlined">search</span>
                 <span class="material-icons-outlined">account_circle</span>
                 <span className="userName">Nombre de Usuario</span>
                 <span class="material-icons-outlined">logout</span>
             </div>
+        </div>
+        )}
         </div>
     );
 }
